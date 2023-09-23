6de7b238f4458c63c532f32483cd72f3298253b4
Noel Sariñena <noelsarinena@ceroca.cat>
2023-09-23 22:28:31 +0200

    Commit 7: Guardado...

--- Scripts/loadScreen.js
+++ Scripts/loadScreen.js
@@ -1,5 +1,5 @@
 function scrollToElement() {
-    var elemento = document.getElementById("#");
+    var elemento = document.getElementById("sec-presentation");
     if (elemento) {
         elemento.scrollIntoView();
     }
