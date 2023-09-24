9925b4d65dec2f48547ef22c5d53b97e7156292f
Noel Sariñena <noelsarinena@ceroca.cat>
2023-09-24 13:22:52 +0200

    Commit 8: Traducción

---
+++ Scripts/languageScript.js
@@ -0,0 +1,64 @@
+
+// Function that translates the language
+function changeLanguage(language) {
+    const translations = {
+        es: {
+            presentation_btnProjects: "PROYECTOS",
+            presentation_initialText: "Saludos, soy",
+            presentation_nameText: "NOEL SARIÑENA",
+            presentation_subnameText: "Full-stack developer líder de varios proyectos personales.",
+            headerAbout: "SOBRE MÍ",
+            headerProjects: "PROYECTOS",
+            headerResume: "CURRÍCULUM",
+            headerContact: "CONTACTO",
+            headerLanguage: "IDIOMA"
+        },
+        en: {
+            presentation_btnProjects: "PROJECTS",
+            presentation_initialText: "Hey, I am",
+            presentation_nameText: "NOEL SARIÑENA",
+            presentation_subnameText: "Lead full-stack developer of several personal projects.",
+            headerAbout: "ABOUT",
+            headerProjects: "PROJECTS",
+            headerResume: "RESUME",
+            headerContact: "CONTACT",
+            headerLanguage: "LANGUAGE"
+        },
+        // More traductions if necessary
+    };
+
+    // Get the IDs of texts who will be translated
+    const presentation_btnProjects = document.getElementById("presentation_btnProjects");
+    const presentation_initialText = document.getElementById("presentation_initialText");
+    const presentation_nameText = document.getElementById("presentation_nameText");
+    const presentation_subnameText = document.getElementById("presentation_subnameText");
+    const headerAbout = document.getElementById("headerAbout");
+    const headerProjects = document.getElementById("headerProjects");
+    const headerResume = document.getElementById("headerResume");
+    const headerContact = document.getElementById("headerContact");
+    const headerLanguage = document.getElementById("headerLanguage");
+
+    // Verify traduction tag
+    if (translations[language]) {
+        presentation_btnProjects.textContent = translations[language].presentation_btnProjects;
+        presentation_initialText.textContent = translations[language].presentation_initialText;
+        presentation_nameText.textContent = translations[language].presentation_nameText;
+        presentation_subnameText.textContent = translations[language].presentation_subnameText;
+        headerAbout.textContent = translations[language].headerAbout;
+        headerProjects.textContent = translations[language].headerProjects;
+        headerResume.textContent = translations[language].headerResume;
+        headerContact.textContent = translations[language].headerContact;
+        headerLanguage.textContent = translations[language].headerLanguage;
+    }
+}
+
+// Event that transalte the page.
+document.addEventListener("DOMContentLoaded", function () {
+    const languageDropdown = document.querySelector(".dropdown-menu");
+    languageDropdown.addEventListener("click", function (event) {
+        if (event.target.getAttribute("data-language")) {
+            const selectedLanguage = event.target.getAttribute("data-language");
+            changeLanguage(selectedLanguage);
+        }
+    });
+});
